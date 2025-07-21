818512482f2e8f0dc61dae46f6835006d59acb2f
Setsumi <butkovvlad2001@gmial.com>
2025-07-21 21:21:34 +0300

    Классная работа(d21m07) - Тема:1)Многопоточность с помощью threading(main.py). Разбор ошибки, смотреть: 222. 2) Асинхронный код(bonus.py). Дополнен кодом, кратким разбором асинхрона

--- d21m07/bonus.py
+++ d21m07/bonus.py
@@ -1,21 +1,37 @@
 import aiohttp, asyncio
+#
+# async def fetch_user_data(user_id:int):
+#     url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url) as response:
+#             if response.status == 200:
+#                 data = await response.json()
+#                 print(f"Пользователь {user_id}: {data['name']}")
+#             else:
+#                 print(f"Ошибка запроса пользователя: {user_id}")
+#
+# async def main():
+#     tasks = []
+#     for user_id in range(1, 11):
+#         task = asyncio.create_task(fetch_user_data(user_id))
+#         tasks.append(task)
+#
+#     await asyncio.gather(*tasks)
+#
+# asyncio.run(main())
 
-async def fetch_user_data(user_id:int):
-    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            if response.status == 200:
-                data = await response.json()
-                print(f"Пользователь {user_id}: {data['name']}")
-            else:
-                print(f"Ошибка запроса пользователя: {user_id}")
+print("one")
+async def fetch_data(url):
+    print(f"Скачивание url...")
+    await asyncio.sleep(2)
+    print(f"Получаем документацию из {url}")
 
+print("two")
 async def main():
-    tasks = []
-    for user_id in range(1, 11):
-        task = asyncio.create_task(fetch_user_data(user_id))
-        tasks.append(task)
-
-    await asyncio.gather(*tasks)
-
+    task1 = asyncio.create_task(fetch_data("https://jopa.org/data1"))
+    task2 = asyncio.create_task(fetch_data("https://jopa.org/data1"))
+    await task1
+    await task2
+print("three")
 asyncio.run(main())
+print("four")
