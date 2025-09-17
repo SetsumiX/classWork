9f70eaeb9b7e94a306c9c29465a712148ac9a5e5
Setsumi <butkovvlad2001@gmial.com>
2025-09-17 21:25:18 +0300

    Классная работа. d015m09. Тема: Возвращение к html, js, и создание сервера.

--- d15m09/framework/fastapi/five_17_09/main.py
+++ d15m09/framework/fastapi/five_17_09/main.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates # jinja2 устанавливается отдельно
 from fastapi.responses import HTMLResponse
 import uvicorn
+from datetime import datetime
 
 app = FastAPI()
 
@@ -11,25 +12,37 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def show_page(request: Request):
-    data = {
-        "welcome": {
-            "message": "Greeting!",
-            "user": "Kiril",
-            "time_is_it": "morning",
-        },
-        "profile": {
-            "email": "killreal@gmail.com",
-            "role": "user",
-            "be_online": "2024-02-15",
-        },
-    }
     return templates.TemplateResponse(
         "./index.html",
-        {
-            "request": request,
-            "data": data
-        }
+        {"request": request}
     )
+
+@app.get("/api/welcome")
+async def get_welcome():
+    return {
+        "message": "Хай",
+        "user": "Чел",
+        "greeting_time": "утро",
+        "time_temp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
+
+@app.get("/api/profile")
+async def get_profile():
+    return {
+        "email": "gigli@gmail.com",
+        "role": "guest",
+        "last_login": "2024-10-15",
+        "login_count": ["read", "write", "delete"],
+    }
+
+@app.get("/api/statistic")
+async def get_statistic():
+    return {
+        "total_users": 125,
+        "active_users": 20,
+        "new_users_week": 3,
+        "system": "oneline",
+    }
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000,
